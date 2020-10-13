523edb23b366fae173facb94711e642059c9aa89
hannata <hanna_tawk_1998@hotmail.com>
2020-10-13 12:04:02 +0300

    added the export to pdf

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import html2canvas from 'html2canvas';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-main',
@@ -11,5 +13,27 @@ export class MainComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  generatePdf(data) {
+    html2canvas(data, { allowTaint: true }).then(canvas => {
+     let HTML_Width = canvas.width;
+     let HTML_Height = canvas.height;
+     let top_left_margin = 15;
+     let PDF_Width = HTML_Width + (top_left_margin * 2);
+     let PDF_Height = (PDF_Width * 1.5) + (top_left_margin * 2);
+     let canvas_image_width = HTML_Width;
+     let canvas_image_height = HTML_Height;
+     let totalPDFPages = Math.ceil(HTML_Height / PDF_Height) - 1;
+     canvas.getContext('2d');
+     let imgData = canvas.toDataURL("image/jpeg", 1.0);
+     let pdf = new jsPDF('p', 'pt', [PDF_Width, PDF_Height]);
+     pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin, canvas_image_width, canvas_image_height);
+     for (let i = 1; i <= totalPDFPages; i++) {
+       pdf.addPage([PDF_Width, PDF_Height], 'p');
+       pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height * i) + (top_left_margin * 4), canvas_image_width, canvas_image_height);
+     }
+      pdf.save("HTML-Document.pdf");
+   });
+ }
 
 }
